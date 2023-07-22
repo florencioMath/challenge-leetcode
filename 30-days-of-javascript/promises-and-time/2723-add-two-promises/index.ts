7b48a5687c1f1edd6f0622cff306fd53ac43415b
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-07-22 12:28:18 -0300

    feat: Promises and Time. part 01.

---
+++ 30-days-of-javascript/promises-and-time/2723-add-two-promises/index.ts
@@ -0,0 +1,12 @@
+async function addTwoPromises(
+  promise1: Promise<number>,
+  promise2: Promise<number>
+): Promise<number> {
+  const [value1, value2] = await Promise.all([promise1, promise2]);
+  return value1 + value2;
+}
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
