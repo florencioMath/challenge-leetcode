bdbf27fcbedb887214e3cf243c95d538c447af61
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-10-23 22:05:10 -0300

    feat: Max Consecutive Ones.

---
+++ arrays-101/max-consecutive-ones/max-consecutive-ones.ts
@@ -0,0 +1,18 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let consecutiveOne: number = 0;
+  let maximumOne: number = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      consecutiveOne++;
+    } else {
+      consecutiveOne = 0;
+    }
+
+    if (consecutiveOne > maximumOne) {
+      maximumOne = consecutiveOne;
+    }
+  }
+
+  return maximumOne;
+}
