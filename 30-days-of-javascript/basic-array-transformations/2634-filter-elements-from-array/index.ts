8e9e41e4b86dd8e4ecae89de9fc1a590186dd667
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2024-08-25 21:38:45 -0300

    fix: Removed old files.

--- 30-days-of-javascript/basic-array-transformations/2634-filter-elements-from-array/index.ts
+++
@@ -1,9 +0,0 @@
-function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
-  const filteredArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i], i)) {
-      filteredArr.push(arr[i]);
-    }
-  }
-  return filteredArr;
-}
--- 30-days-of-javascript/json/2677-chunk-array/index.ts
+++
@@ -1,9 +0,0 @@
-function chunk(arr: any[], size: number): any[][] {
-  const chunked = [];
-
-  for (let i = 0; i < arr.length; i += size) {
-    chunked.push(arr.slice(i, i + size));
-  }
-
-  return chunked;
-}
