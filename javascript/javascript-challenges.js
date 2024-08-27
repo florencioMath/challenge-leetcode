74786bc69a2edb1249bde4c2ad94b287886b1045
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2024-08-27 19:38:30 -0300

    feat: 0001. Two Sum.

---
+++ javascript/javascript-challenges.js
@@ -0,0 +1,26 @@
+// Challenge - 0001. Two Sum
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var twoSum = function(nums, target) {
+//   for (let i = 0; nums.length > i; i++) {
+//       for (let j = i + 1; j < nums.length; j++) {
+//           if(nums[i] + nums[j] === target) return [i,j];
+//       }
+//   }
+//   return [];
+//
+//   // const map = {};
+//   // for (let i = 0; i < nums.length; i++) {
+//   //     const complement = target - nums[i];
+//   //     if (map[complement] !== undefined) {
+//   //         return [map[complement], i];
+//   //     }
+//   //     map[nums[i]] = i;
+//   // }
+//   // return [];
+// };
+
+// *****************************************************************************//
