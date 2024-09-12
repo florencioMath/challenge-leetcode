96dad6e2c72aa67418891a979e21cf9530db72eb
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2024-09-12 13:15:47 -0300

    feat: 0013. Roman to Integer.

--- javascript/javascript-challenges.js
+++ javascript/javascript-challenges.js
@@ -46,3 +46,34 @@
 // };
 
 // *****************************************************************************//
+
+// Challenge - 0013. Roman to Integer
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var romanToInt = function(s) {
+//   let sum = 0;
+//   for (let i = 0; i < s.length; i++) {
+//       const cur = symbols[s[i]];
+//       const next = symbols[s[i + 1]];
+//       if(cur < next) {
+//           sum += next - cur;
+//           i++;
+//       } else {
+//           sum += cur;
+//       }
+//   }
+//   return sum;
+// };
+// const symbols = {
+//   I: 1,
+//   V: 5,
+//   X: 10,
+//   L: 50,
+//   C: 100,
+//   D: 500,
+//   M: 1000
+// }
+
+// *****************************************************************************//
